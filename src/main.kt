1c9375d783a7ede0608241f0330f3d109906d7cf
Brian <bburger@students.mchenry.edu>
2020-02-05 18:09:14 -0600

    Variables

---
+++ src/main.kt
@@ -0,0 +1,31 @@
+fun main() {
+    //Numbers
+    var myByte : Byte = 2
+    var myShort : Short = 50
+    var myInt : Int = 255
+    var myLong : Long = 6422
+    var myFloat = 1.25
+    var myDouble = 4546.2345
+
+    //Boolean
+    var myBoolean = true;
+
+    //Strings
+    var myString = "This is \"my\" string \n "
+    var myChar = 'B'
+
+
+    //Printout
+    println("=============Numbers=====================")
+    println("byte demo: "+myByte)
+    println("short demo: "+myShort)
+    println("int demo: "+myInt)
+    println("long demo: "+myLong)
+    println("float demo: "+myFloat)
+    println("double demo: "+myDouble)
+    println("=============Boolean=====================")
+    println("boolean demo: "+myBoolean)
+    println("=============Strings=====================")
+    println("String demo: "+myString)
+    println("Char demo: "+myChar)
+}
